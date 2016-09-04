3f532321a880677f4fc292336099b17b138371dc
richelbilderbeek <richel@richelbilderbeek.nl>
2016-09-04 11:36:33 +0200

    Created nice demo

--- my_functions.h
+++ my_functions.h
@@ -1,6 +1,13 @@
 #ifndef MY_FUNCTIONS_H
 #define MY_FUNCTIONS_H
 
-int add(const int i, const int j) noexcept;
+#include <vector>
+
+///Calculate the mean.
+///Will throw if the input is empty
+double calc_mean(const std::vector<double>& v);
+
+///Determine if a number is odd
+bool is_odd(const int i) noexcept;
 
 #endif // MY_FUNCTIONS_H
