9f11741bba357f5f6ff57be015a68b508f882ff9
richelbilderbeek <richel@richelbilderbeek.nl>
2016-09-04 17:11:01 +0200

    Simplified build.sh and do_oclint.sh

--- do_oclint.sh
+++ do_oclint.sh
@@ -1,48 +1,16 @@
 #!/bin/bash
 
-cpp_files=`ls *.cpp | egrep -v "^qrc_.*\.cpp$" | egrep -v "^moc_.*\.cpp$" | egrep -v "^.*_test\.cpp$"`
-h_files=`ls *.h | egrep -v "^ui_.*\.h$"`
+cpp_files=`ls *.cpp | egrep -v "^.*_test\.cpp$"`
+h_files=`ls *.h`
 
 ./oclint-0.10.3/bin/oclint -o oclint.log \
   -disable-rule ShortVariableName \
   $cpp_files \
   $h_files \
   -- \
-  -c -std=c++11 -fPIC \
-  -I../RibiClasses/CppAbout \
-  -I../ConceptMap \
-  -I../RibiClasses/CppContainer \
-  -I../RibiClasses/CppCounter \
-  -I../RibiClasses/CppFuzzy_equal_to \
-  -I../RibiClasses/CppGeometry \
-  -I../RibiClasses/CppHelp \
-  -I../RibiClasses/CppTestTimer \
-  -I../RibiClasses/CppFileIo \
-  -I../RibiClasses/CppMenuDialog \
-  -I../RibiClasses/CppPlane \
-  -I../RibiClasses/CppQtAboutDialog \
-  -I../RibiClasses/CppQtArrowItem \
-  -I../QtConceptMap \
-  -I../RibiClasses/CppQtHideAndShowDialog \
-  -I../RibiClasses/CppQtKeyboardFriendlyGraphicsView \
-  -I../RibiClasses/CppQtQuadBezierArrowItem \
-  -I../RibiClasses/CppQtRoundedEditRectItem \
-  -I../RibiClasses/CppQtRoundedRectItem \
-  -I../RibiClasses/CppQtScopedDisable \
-  -I../RibiClasses/CppRibiRegex \
-  -I../RibiClasses/CppTrace \
-  -I../RibiClasses/CppXml \
-  -I../BoostGraphTutorial/BoostGraphTutorial \
-  -I../StyleSheetSetter \
-  -I../RibiLibraries/Apfloat \
+  -c -std=c++11 \
   -I/usr/include/c++/5 \
-  -I/usr/include/x86_64-linux-gnu/c++/5 \
-  -I/usr/include/qt4 \
-  -I/usr/include/qt4/QtCore \
-  -I/usr/include/qt4/QtGui \
-  -I/usr/include/qt4/QtPrintSupport \
-  -I/usr/include/qt4/QtTest
-
+  -I/usr/include/x86_64-linux-gnu/c++/5
 cat oclint.log
 
 # Will be 0 if success
